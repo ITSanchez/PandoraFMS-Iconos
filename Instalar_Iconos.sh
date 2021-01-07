f04e188ebe2ea78da24ba75944e069316ec4cabe
ITSanchez <gsanchez@itsanchez.com.ar>
2021-01-07 14:43:55 -0300

    Version 1.0

--- Instalar_Iconos.sh
+++ Instalar_Iconos.sh
@@ -9,6 +9,7 @@ cp iconos.zip /var/www/html/pandora_console/images/console/icons
 cd /var/www/html/pandora_console/images/console/icons
 unzip iconos.zip
 rm -r iconos.zip 
+chown apache.apache *.*
 echo "Reiniciando apache..."
 systemctl restart httpd
 echo "Instalacion Finalizada"
